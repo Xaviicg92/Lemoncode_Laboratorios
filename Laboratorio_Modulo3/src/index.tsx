7c4b504334fd8f7b673cbd209ae3761378838adf
Javier Cambón <cg_javi@hotmail.com>
2022-12-05 19:37:09 +0100

    modificacion

--- Laboratorio_Modulo3/src/index.tsx
+++ Laboratorio_Modulo3/src/index.tsx
@@ -1,19 +1,14 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
-import {AverageComponent} from "./averageComponent";
-import "./styles.scss"
-import classes from "./styles.scss"
-
+import { createRoot } from "react-dom/client";
+import "./styles.scss";
+import classes from "./styles.scss";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-    <div>
-        <a href="#">
-        <span>Hello World</span>
-        <div class={classes.liquid}></div>
-        </a>
-    </div>
-    
-    
+  <div>
+    <a href="#">
+      <span>Hello World</span>
+      <div class={classes.liquid}></div>
+    </a>
+  </div>
 );
-
